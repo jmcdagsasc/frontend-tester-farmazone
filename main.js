1034390a1e4834dd54e4be77737c9af20546137c
jmcdagsasc <82073613+jmcdagsasc@users.noreply.github.com>
2021-04-12 10:33:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
 $(document).ready(function () {
   const FARMAZONE_API =
     "https://hardcore-poitras-19aebd.netlify.app/.netlify/functions/getcategories";
+  let business_id = 0;
 
   function apiCall() {
     console.log("Se ejecuto");
-    $.getJSON(FARMAZONE_API, function (result) {
-      console.log("success");
-      let container = $("#categories");
-      for (let i = 0; i < result.categoriesLength; i++) {
-        const strCategory = JSON.stringify(result.categories[i].name).replace(
-          /\"/g,
-          ""
-        );
-        let category = $("<h5></h5>").text(strCategory);
-        container.append(category);
-      }
-    })
-      .fail(function () {
-        console.log("error");
-      })
-      .always(function () {
-        console.log("complete");
-      });
+    $.post(
+      FARMAZONE_API,
+      JSON.stringify({ business_id }),
+      function (result) {
+        console.log("success");
+        let container = $("#categories");
+        container.empty();
+        for (let i = 0; i < result.categoriesLength; i++) {
+          const strCategory = JSON.stringify(result.categories[i].name).replace(
+            /\"/g,
+            ""
+          );
+          let category = $("<h5></h5>").text(strCategory);
+          container.append(category);
+        }
+      },
+      "json"
+    );
   }
-  apiCall();
+
+  function selectStore() {
+    if ($("#business_id").val() == false) {
+      business_id = 149;
+    } else {
+      business_id = parseInt($("#business_id").val());
+    }
+
+    console.log(
+      "business_id : " + business_id + ", type: " + typeof business_id
+    );
+  }
+
+  $("#business-id-btn").click(function () {
+    selectStore();
+    apiCall();
+  });
+
   console.log("Hello there!");
 });
